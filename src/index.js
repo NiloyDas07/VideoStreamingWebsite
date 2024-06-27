3f1586082ffcfee3bc874d22106347fead3460b4
NSD <niloysd7@gmail.com>
2024-06-27 22:17:31 +0530

    Some utils

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(() => {
+  app.on("error", (error) => {
+    console.log("Error: ", error);
+  })
+
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server running on port ${process.env.PORT || 8000}`);
+  })
+})
+.catch((error) => {
+  console.log("MongoDB connection FAILED: ", error);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,40 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      sucess: false,
+      message: error.message,
+    });
+  }
+};
+
+Equivalent to onst asyncHandler = (fn) => {() => {}} 
+
+or
+
+function asyncHandler(fn) {
+  // Return a new asynchronous function
+  return async function (req, res, next) {
+    try {
+      // Execute the passed-in function (fn) with the request, response, and next arguments
+      await fn(req, res, next);
+    } catch (error) {
+      // If an error occurs, catch it and send a response with the error information
+      res.status(error.code || 500).json({
+        success: false, // Indicate that the operation was not successful
+        message: error.message, // Include the error message in the response
+      });
+    }
+  };
+}
+*/
+
+export { asyncHandler };
