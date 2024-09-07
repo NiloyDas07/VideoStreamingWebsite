dd14ee43cc40076f05a83af1ba0777298c2c5d4b
NSD <niloysd7@gmail.com>
2024-09-07 12:46:51 +0530

    Updating server url

--- client/src/utils/axios.util.js
+++ client/src/utils/axios.util.js
@@ -1,7 +1,27 @@
 import axios from "axios";
 
+const mode = import.meta.env.MODE;
+const devURL = import.meta.env.VITE_DEV_API_URL;
+const prodURL = import.meta.env.VITE_PROD_API_URL;
+
+let baseURL;
+
+switch (mode) {
+  case "development":
+    baseURL = devURL;
+    break;
+
+  case "production":
+    baseURL = prodURL;
+    break;
+
+  default:
+    baseURL = devURL;
+    break;
+}
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7000/api/v1",
+  baseURL,
   withCredentials: true,
 });
 
